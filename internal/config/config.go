2490ddda3b6084e21a62fa0c034de092fe88a101
antonio mauricio <antonioimportant@gmail.com>
2024-08-10 11:43:55 -0300

    feat: moves configuration files to internal/config dir + updates config.go path to config files

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,6 +6,43 @@ import (
 
 var PermissionIDS []int64 = []int64{6105709676, 7496811705}
 
+<<<<<<< HEAD
 type ConfigJsonPaylod struct {
 	Dates []types.Birthday `json:"dates"`
+=======
+func GetBirthdays() []types.Birthday {
+	var birhtdaysPaylod struct {
+		Dates []types.Birthday `json:"dates"`
+	}
+
+	birthdaysFile, err := os.ReadFile(path.Join(currentDir, "internal", "config", "birthdays.json"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(birthdaysFile, &birhtdaysPaylod)
+	if err != nil {
+		panic(err)
+	}
+
+	return birhtdaysPaylod.Dates
+}
+
+func GetAllowedUserIDS() []int64 {
+	var allowedUsersPayload struct {
+		IDS []int64 `json:"allowed_ids"`
+	}
+
+	allowedIDSFile, err := os.ReadFile(path.Join(currentDir, "internal", "config", "allowed_user_ids.json"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(allowedIDSFile, &allowedUsersPayload)
+	if err != nil {
+		panic(err)
+	}
+
+	return allowedUsersPayload.IDS
+>>>>>>> af698d6 (feat: moves configuration files to internal/config dir + updates config.go path to config files)
 }
