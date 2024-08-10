a0f451e25d8b6e3471a893941dd9f4264c8c541d
antonio mauricio <antonioimportant@gmail.com>
2024-08-10 19:21:27 -0300

    feat: add cron job to fire once every day

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log/slog"
+	"time"
+
 	"github.com/Dedo-Finger2/fam-birthday-bot/internal/config"
 	"github.com/Dedo-Finger2/fam-birthday-bot/internal/utils"
 )
@@ -8,14 +11,25 @@ import (
 func main() {
 	bot := config.SetupBot()
 
-	birthdates := utils.GetBirthdays()
+	for {
+		now := time.Now().Local()
 
-	currentDateDDMM := utils.GetCurrentDateDDMM()
+		if now.Hour() == 19 && now.Minute() == 15 {
+			birthdates := utils.GetBirthdays()
+			currentDateDDMM := utils.GetCurrentDateDDMM()
 
-	for _, birthdate := range birthdates {
-		if birthdate.Date == currentDateDDMM {
-			utils.SendMessage(birthdate.Date, bot)
-			return
+			for _, birthdate := range birthdates {
+				if birthdate.Date == currentDateDDMM {
+					utils.SendMessage(birthdate.Date, bot)
+					break
+				}
+			}
+
+			slog.Info("Messages sent! Next validation in 24 hours.")
+			time.Sleep(time.Hour * 24)
+		} else {
+			slog.Warn("it is not 5 am yet, waiting 1 minute before trying again...")
+			time.Sleep(time.Minute)
 		}
 	}
 }
