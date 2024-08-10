c31f5c8921017cb0cef23fbde09ebbc792e24f05
antonio mauricio <antonioimportant@gmail.com>
2024-08-10 16:56:53 -0300

    feat: add date parameter to GetUserNameComplement util func

--- internal/utils/get_users_name_complement.go
+++ internal/utils/get_users_name_complement.go
@@ -2,12 +2,16 @@ package utils
 
 import "fmt"
 
-func GetUserNameComplement() []string {
+func GetUserNameComplement(date string) []string {
 	birthdates := GetBirthdays()
 
 	people := []string{}
 
 	for _, birthdate := range birthdates {
+		if birthdate.Date != date {
+			continue
+		}
+
 		for _, person := range birthdate.People {
 			people = append(people, fmt.Sprintf("%s - %s", person.Name, person.Complement))
 		}
