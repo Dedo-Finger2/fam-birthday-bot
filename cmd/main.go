f0291bdccc1a23b1d00d890cea50e2d30874904c
antonio mauricio <antonioimportant@gmail.com>
2024-08-10 17:08:33 -0300

    feat: add checking current date and sending message to registered users logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/Dedo-Finger2/fam-birthday-bot/internal/config"
+	"github.com/Dedo-Finger2/fam-birthday-bot/internal/utils"
+)
 
+func main() {
+	bot := config.SetupBot()
+
+	birthdates := utils.GetBirthdays()
+
+	currentDateDDMM := utils.GetCurrentDateDDMM()
+
+	for _, birthdate := range birthdates {
+		if birthdate.Date == currentDateDDMM {
+			utils.SendMessage(birthdate.Date, bot)
+			return
+		}
+	}
 }
